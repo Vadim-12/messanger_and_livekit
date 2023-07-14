883a603933d590711dbc55a4fbf491580832d30b
Vadim-12 <agarkov.vadim@gmail.com>
2023-07-14 22:58:45 +0300

    fix(v1): console.log(vars) in login page

--- services/client/src/pages/LoginPage/LoginPage.tsx
+++ services/client/src/pages/LoginPage/LoginPage.tsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { colors } from 'theme/colors';
 import { ROOM_ROUTE } from 'config/const/routes/website/room';
 import { ROOM_IS_FULL_ROUTE } from 'config/const/routes/website/room/isFull';
+import { vars } from 'config/vars';
 
 interface Props {
   name: string;
@@ -18,6 +19,8 @@ const LoginPage: React.FC<Props> = ({ name, setName }) => {
   const [validationError, setValidationError] = useState<boolean>(false);
   const [focus, setFocus] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  console.log(vars);
 
   const isValidate = useMemo(() => {
     return name.length > 0;
